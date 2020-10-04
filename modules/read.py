970dcc831d6240a5653dff10797e4b16294bb1a3
tichnas <sanchit.arora.2002@gmail.com>
2020-10-04 20:45:36 +0530

    Integrate Reading Data

--- modules/read.py
+++ modules/read.py
@@ -1,115 +1,127 @@
-def readBrands(cur, con):
+from tabulate import tabulate
+
+def printResult(cur):
+    result = cur.fetchall()
+
+    if len(result) != 0:
+        header = result[0].keys()
+        rows =  [x.values() for x in result]
+        print(tabulate(rows, header, tablefmt = 'grid'))
+    else:
+        print("No Data!")
+
+def readBrands(con, cur):
     try:
         query = 'SELECT * FROM brand;'
         cur.execute(query)
-        con.commit()
+        printResult(cur)
     except Exception as e:
         con.rollback()
         print(e)
         print("\nError: READ FAILED!\n")
 
 
-def readActors(cur, con):
+def readActors(con, cur):
     try:
         query = 'SELECT * FROM actor NATURAL JOIN person NATURAL JOIN phone;'
         cur.execute(query)
-        con.commit()
+        printResult(cur)
     except Exception as e:
         con.rollback()
         print(e)
         print("\nError: READ FAILED!\n")
 
 
-def readDirectors(cur, con):
+def readDirectors(con, cur):
     try:
         query = 'SELECT * FROM director NATURAL JOIN person NATURAL JOIN phone;'
         cur.execute(query)
-        con.commit()
+        printResult(cur)
     except Exception as e:
         con.rollback()
         print(e)
         print("\nError: READ FAILED!\n")
 
 
-def readChannels(cur, con):
+def readChannels(con, cur):
     try:
         query = 'SELECT * FROM channel;'
         cur.execute(query)
-        con.commit()
+        printResult(cur)
     except Exception as e:
         con.rollback()
         print(e)
         print("\nError: READ FAILED!\n")
 
 
-def readShows(cur, con):
+def readShows(con, cur):
     try:
         query = 'SELECT * FROM `show`;'
         cur.execute(query)
-        con.commit()
+        printResult(cur)
     except Exception as e:
         con.rollback()
         print(e)
         print("\nError: READ FAILED!\n")
 
 
-def readProducts(cur, con):
+def readProducts(con, cur):
     try:
         query = 'SELECT * FROM product;'
         cur.execute(query)
-        con.commit()
+        printResult(cur)
     except Exception as e:
         con.rollback()
         print(e)
         print("\nError: READ FAILED!\n")
 
 
-def readAds(cur, con):
+def readAds(con, cur):
     try:
         query = 'SELECT production.*, duration FROM production INNER JOIN ad ON ad.serialNo = production.adSerialNo;'
         cur.execute(query)
-        con.commit()
+        printResult(cur)
     except Exception as e:
         con.rollback()
         print(e)
         print("\nError: READ FAILED!\n")
 
 
-def maxProdCost(cur, con):
+def maxProdCost(con, cur):
     try:
-        query = 'SELECT MAX(TotalCost.NetCost) AS HighestProductionCost FROM (SELECT SUM(production.productionCost) AS NetCost FROM production INNER JOIN product ON ( product.name = production.productName AND product.brandName = production.brandName ) GROUP BY product.brandname) AS TotalCost;'
+        query = 'SELECT MAX(TotalCost.NetCost) AS `Highest Production Cost` FROM (SELECT SUM(production.productionCost) AS NetCost FROM production INNER JOIN product ON ( product.name = production.productName AND product.brandName = production.brandName ) GROUP BY product.brandname) AS TotalCost;'
         cur.execute(query)
-        con.commit()
+        printResult(cur)
     except Exception as e:
         con.rollback()
         print(e)
         print("\nError: READ FAILED!\n")
 
 
-def adBill(cur, con):
+def adBill(con, cur):
     serialNo = input("Ad Serial No: ")
     try:
         query = f'SELECT production.productionCost + SUM(channel.basePrice) + SUM(displayedBetween.timesShown * ad.duration * `show`.surcharge) AS Bill FROM (((( displayedBetween INNER JOIN ad ON displayedBetween.adSerialNo = ad.serialNo) INNER JOIN `show` ON displayedBetween.showDate = `show`.date AND displayedBetween.showStartTime = `show`.startTime AND displayedBetween.channelName = `show`.channelName) INNER JOIN production ON ad.serialNo = production.adSerialNo) INNER JOIN channel ON `show`.channelName = channel.channelName) WHERE displayedBetween.adSerialNo = {serialNo};'
         cur.execute(query)
-        con.commit()
+        printResult(cur)
     except Exception as e:
         con.rollback()
         print(e)
         print("\nError: READ FAILED!\n")
 
 
-def showList(cur, con):
+def showList(con, cur):
     try:
         query = 'SELECT (`show`.surcharge * SUM(ad.duration)) AS Earning, `show`.date, `show`.startTime, `show`.channelName, `show`.name FROM (( displayedBetween INNER JOIN ad ON displayedBetween.adSerialNo = ad.serialNo) INNER JOIN `show` ON displayedBetween.showDate = `show`.date AND displayedBetween.showStartTime = `show`.startTime AND displayedBetween.channelName = `show`.channelName) GROUP BY `show`.date, `show`.startTime, `show`.channelName, `show`.name ORDER BY Earning DESC;'
         cur.execute(query)
-        con.commit()
+        printResult(cur)
     except Exception as e:
         con.rollback()
         print(e)
         print("\nError: READ FAILED!\n")
 
 
-def actorsByFeatures(cur, con):
+def actorsByFeatures(con, cur):
     print("Enter physical features range: ")
     minHeight = input("Minimum Height (in cm): ")
     maxHeight = input("Maximum Height (in cm): ")
@@ -117,79 +129,79 @@ def actorsByFeatures(cur, con):
     maxWeight = input("Maximum Weight (in kg): ")
     gender = input('Gender (Male or Female or Other):')
     try:
-        query = f'SELECT * FROM person INNER JOIN actor ON person.aadharCard = actor.aadharCard WHERE person.gender = "{gender}" AND ( actor.height BETWEEN {minHeight} AND {maxHeight} ) AND ( actor.weight BETWEEN {minWeight} AND ${maxWeight} );'
+        query = f'SELECT * FROM person INNER JOIN actor ON person.aadharCard = actor.aadharCard WHERE person.gender = "{gender}" AND ( actor.height BETWEEN {minHeight} AND {maxHeight} ) AND ( actor.weight BETWEEN {minWeight} AND {maxWeight} );'
         cur.execute(query)
-        con.commit()
+        printResult(cur)
     except Exception as e:
         con.rollback()
         print(e)
         print("\nError: READ FAILED!\n")
 
 
-def avgProduction(cur, con):
+def avgProduction(con, cur):
     try:
         query = 'SELECT AVG(productionCost) FROM production;'
         cur.execute(query)
-        con.commit()
+        printResult(cur)
     except Exception as e:
         con.rollback()
         print(e)
         print("\nError: READ FAILED!\n")
 
 
-def maxPreferred(cur, con):
+def maxPreferred(con, cur):
     try:
         query = 'SELECT brandname, COUNT(brandname) AS count_prefer FROM prefers GROUP BY brandname ORDER BY count_prefer DESC LIMIT 1;'
         cur.execute(query)
-        con.commit()
+        printResult(cur)
     except Exception as e:
         con.rollback()
         print(e)
         print("\nError: READ FAILED!\n")
 
 
-def searchShow(cur, con):
+def searchShow(con, cur):
     text = input("Enter part of name: ")
     try:
         query = f'SELECT date, startTime, channelName, name FROM `show` WHERE name like "%{text}%";'
         cur.execute(query)
-        con.commit()
+        printResult(cur)
     except Exception as e:
         con.rollback()
         print(e)
         print("\nError: READ FAILED!\n")
 
 
-def searchActor(cur, con):
+def searchActor(con, cur):
     text = input("Enter part of name: ")
     try:
         query = f'SELECT actor.aadharCard AS AadharCard, name FROM person INNER JOIN actor ON person.aadharCard = actor.aadharCard WHERE person.name like "%{text}%"'
         cur.execute(query)
-        con.commit()
+        printResult(cur)
     except Exception as e:
         con.rollback()
         print(e)
         print("\nError: READ FAILED!\n")
 
 
-def surchargeLessThan(cur, con):
+def surchargeLessThan(con, cur):
     maxSurcharge = input("Enter part of name: ")
     try:
         query = f'SELECT name AS showName FROM `show` WHERE `show`.surcharge < {maxSurcharge};'
         cur.execute(query)
-        con.commit()
+        printResult(cur)
     except Exception as e:
         con.rollback()
         print(e)
         print("\nError: READ FAILED!\n")
 
 
-def showsForAd(cur, con):
+def showsForAd(con, cur):
     serialNo = input("Ad Serial No: ")
     try:
         query = f'SELECT showDate, showStartTime, channelName FROM showGenre WHERE name IN (SELECT name As AdGenrename FROM ad INNER JOIN adGenre on serialNo = adSerialNo WHERE serialNo = {serialNo});'
         cur.execute(query)
-        con.commit()
+        printResult(cur)
     except Exception as e:
         con.rollback()
         print(e)
--- modules/repl.py
+++ modules/repl.py
@@ -1,5 +1,6 @@
 import subprocess as sp
 from .create import *
+from .read import *
 
 
 def printMenu():
@@ -50,25 +51,44 @@ def read_menu(connection, cursor):
     while True:
         _ = sp.call("clear", shell=True)
         print("==============READ MENU==================")
-        print("0. Back\n1. Actor\n2. Director\n3. Brand\n4. Channel\n5. Show\n6. Product\n7. Ad \
-            \n8. Maximum of sum of the contract money of brand\n9. Bill for Ad\n10. Shows list by amount \
-            \n11. Actors with Physical features\n12. Average ad production cost\
-            \n13. Maximum preferred brands\n14. Partial text search for show\n15. Get best shows for an ad\n")
+        print("0. Back\n1. Get all Actors\n2. Get all Directors\n3. Get all Brands\n4. Get all Channels\n5. Get all Shows\n6. Get all Products\n7. Get all Ads\n8. Maximum of sum of the contract money of brand\n9. Bill for Ad\n10. Shows list by amount\n11. Actors with Physical features\n12. Average ad production cost\n13. Maximum preferred brands\n14. Partial text search for show\n15. Get best shows for an ad\n16. Partial text search for actor\n17. Shows with surcharge less than a value\n")
         ch = input("Enter your choice > ")
         if ch == '0':
             return
         elif ch == '1':
-            addActor(connection, cursor)
+            readActors(connection, cursor)
         elif ch == '2':
-            addDirector(connection, cursor)
+            readDirectors(connection, cursor)
         elif ch == '3':
-            addBrand(connection, cursor)
+            readBrands(connection, cursor)
         elif ch == '4':
-            addChannel(connection, cursor)
+            readChannels(connection, cursor)
         elif ch == '5':
-            addShow(connection, cursor)
+            readShows(connection, cursor)
+        elif ch == '6':
+            readProducts(connection, cursor)
         elif ch == '7':
-            addProduction(connection, cursor)
+            readAds(connection, cursor)
+        elif ch == '8':
+            maxProdCost(connection, cursor)
+        elif ch == '9':
+            adBill(connection, cursor)
+        elif ch == '10':
+            showList(connection, cursor)
+        elif ch == '11':
+            actorsByFeatures(connection, cursor)
+        elif ch == '12':
+            avgProduction(connection, cursor)
+        elif ch == '13':
+            maxPreferred(connection, cursor)
+        elif ch == '14':
+            searchShow(connection, cursor)
+        elif ch == '15':
+            showsForAd(connection, cursor)
+        elif ch == '16':
+            searchActor(connection, cursor)
+        elif ch == '17':
+            surchargeLessThan(connection, cursor)
         else:
             print("Invalid option\n")
 
