851566b4b74ae979dc9985d9d27e3b3d9cba5c87
Gurkirat Singh <gurkirat.singh@students.iiit.ac.in>
2020-10-04 16:29:00 +0530

    [Add] read menu

--- modules/repl.py
+++ modules/repl.py
@@ -14,29 +14,62 @@ def repl(connection, cursor):
         ch = input("Enter your choice > ")
         if ch == '1':
             add_menu(connection, cursor)
+        if ch == '4':
+            read_menu(connection, cursor)
         elif ch == '5':
             raise SystemExit
 
 
 def add_menu(connection, cursor):
-    _ = sp.call("clear", shell=True)
+    while True:
+        _ = sp.call("clear", shell=True)
+        print("==============ADD MENU==================")
+        print("0. Back\n1. Actor\n2. Director\n3. Brand\n4. Channel\n5. Show\n6. Product\n7. Ad\n")
+        ch = input("Enter your choice > ")
+        if ch == '0':
+            return
+        elif ch == '1':
+            addActor(connection, cursor)
+        elif ch == '2':
+            addDirector(connection, cursor)
+        elif ch == '3':
+            addBrand(connection, cursor)
+        elif ch == '4':
+            addChannel(connection, cursor)
+        elif ch == '5':
+            addShow(connection, cursor)
+        elif ch == '7':
+            addProduction(connection, cursor)
+        else:
+            print("Invalid option\n")
 
-    print("==============ADD MENU==================")
-    print("0. Back\n1. Actor\n2. Director\n3. Brand\n4. Channel\n5. Show\n6. Product\n7. Ad\n")
-    ch = input("Enter your choice > ")
-    if ch == '0':
-        return
-    elif ch == '1':
-        addActor(connection, cursor)
-    elif ch == '2':
-        addDirector(connection, cursor)
-    elif ch == '3':
-        addBrand(connection, cursor)
-    elif ch == '4':
-        addChannel(connection, cursor)
-    elif ch == '5':
-        addShow(connection, cursor)
-    elif ch == '7':
-        addProduction(connection, cursor)
-    else:
-        print("Invalid option\n")
+        _ = input("Enter a key to continue")
+
+
+def read_menu(connection, cursor):
+    while True:
+        _ = sp.call("clear", shell=True)
+        print("==============READ MENU==================")
+        print("0. Back\n1. Actor\n2. Director\n3. Brand\n4. Channel\n5. Show\n6. Product\n7. Ad \
+            \n8. Maximum of sum of the contract money of brand\n9. Bill for Ad\n10. Shows list by amount \
+            \n11. Actors with Physical features\n12. Average ad production cost\
+            \n13. Maximum preferred brands\n14. Partial text search for show\n15. Get best shows for an ad\n")
+        ch = input("Enter your choice > ")
+        if ch == '0':
+            return
+        elif ch == '1':
+            addActor(connection, cursor)
+        elif ch == '2':
+            addDirector(connection, cursor)
+        elif ch == '3':
+            addBrand(connection, cursor)
+        elif ch == '4':
+            addChannel(connection, cursor)
+        elif ch == '5':
+            addShow(connection, cursor)
+        elif ch == '7':
+            addProduction(connection, cursor)
+        else:
+            print("Invalid option\n")
+
+        _ = input("Enter a key to continue")
